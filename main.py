16c97438cb14ad7f3b8e5ed6f28741efe549a7a1
das1292 <jakub.klos.10@wp.pl>
2021-03-24 09:52:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import klasy
+from klasy import *
+#Zadanie1
+# lista1 = [0+x for x in range(1, 31, 1)]
+# listax2 = [str(x*2) for x in lista1]
+# plik = open("liczya2.txt", "w")
+# plik.writelines(lista2)
