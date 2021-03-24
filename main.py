5cef97216efeefeb6306d8bd61f36e4060944308
das1292 <jakub.klos.10@wp.pl>
2021-03-24 09:55:22 +0100

    Zadanie4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Zadanie1
 # lista1 = [0+x for x in range(1, 31, 1)]
-# listax2 = [str(x*2) for x in lista1]
-# plik = open("liczya2.txt", "w")
+# lista2 = [str(x*2) for x in lista1]
+# plik = open("liczyx2.txt", "w")
 # plik.writelines(lista2)
